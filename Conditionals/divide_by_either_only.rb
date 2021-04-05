115e3ca1a2a8f3c24aaa6fd80c429a9f8827660d
BLaineLaas <blainelaas.bl@gmail.com>
2021-04-05 16:05:17 +0200

    Refined code to make it shorter and added tests

--- Conditionals/divide_by_either_only.rb
+++ Conditionals/divide_by_either_only.rb
@@ -1,13 +1,30 @@
 # Takes number and returns true if the number is divisible by either 3 or 5 not both
 # The Method returns false otherwise
 
-def either_only(number)
+# def either_only(number)
 
-    if number % 3 == 0 && number % 5 != 0
-      puts "true"
-    elsif number % 5 == 0 && number % 3 != 0
-      puts "true"
+#     if number % 3 == 0 && number % 5 != 0
+#       puts "true"
+#     elsif number % 5 == 0 && number % 3 != 0
+#       puts "true"
+#     else
+#       puts "false"
+#     end  
+#   end
+
+# refined above code
+
+def either_only(number)
+    if (number % 3 ==0 || number % 5 ==0) && !(number % 3 == 00 && number % 5 == 0)
+        return true
     else
-      puts "false"
-    end  
-  end
+        return false
+    end
+end
+
+# Test Code
+puts either_only(9) 
+puts either_only(20)
+puts either_only(7) 
+puts either_only(15) 
+puts either_only(30)
