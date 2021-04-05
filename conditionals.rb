00e5503fb356be3ed53dc5f98b2bbad285ba6b69
BLaineLaas <blainelaas.bl@gmail.com>
2021-04-05 15:19:17 +0200

    Updated conditionals to v3

--- conditionals.rb
+++ conditionals.rb
@@ -1,5 +1,5 @@
-
-num = 0
+# Check if number is positive/negative and Even/odd
+num = 8
 
 if num > 0
     puts "Positive"
@@ -7,4 +7,10 @@ elsif num < 0
     puts "Negative" 
 else 
     puts "It is zero!"
+end
+
+if num % 2 == 0
+    puts "Even"
+else
+    puts "Odd"
 end
