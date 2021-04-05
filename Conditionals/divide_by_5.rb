11c3102131a9b63c97067ff1f467170f65476eea
BLaineLaas <blainelaas.bl@gmail.com>
2021-04-05 15:48:22 +0200

    Refined code and commented out previous version

--- Conditionals/divide_by_5.rb
+++ Conditionals/divide_by_5.rb
@@ -1,12 +1,17 @@
 # method to check if number input can be devided by 5
 
+# def is_div_by_5(number)
+#     if number % 5 == 0
+#         return true
+#     else
+#         return false
+#     end
+# end
+
+# Refined version 
 def is_div_by_5(number)
-    if number % 5 == 0
-        return true
-    else
-        return false
-    end
-end
+    return number % 5 == 0
+end 
 
 puts is_div_by_5(10)
 puts is_div_by_5(30)
